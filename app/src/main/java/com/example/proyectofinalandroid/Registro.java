c89479af7da5d70513d67fd00dd5a6a1548f7734
LeoLucasC <125475720+LeoLucasC@users.noreply.github.com>
2025-02-28 07:31:13 -0500

    uwu

--- app/src/main/java/com/example/proyectofinalandroid/Registro.java
+++ app/src/main/java/com/example/proyectofinalandroid/Registro.java
@@ -1,9 +1,6 @@
 package com.example.proyectofinalandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +8,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class login extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registro);
 
 
 
-        // Aplica los insets
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-
-        TextView signUpPrompt = findViewById(R.id.signUpPrompt);
-        signUpPrompt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(login.this, Registro.class);
-                startActivity(intent);
-            }
         });
     }
 }
